2dd22f11852505bb6badf844e18df9c537ff2dd9
danik <kotsovskijdanilo@gmail.com>
2025-12-21 12:29:59 +0200

    divided files, json working

---
+++ main/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace main
+{
+    class Product
+    {
+        public Product(string name, int price, string type)
+        {
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
+    }
+}
--- main/Program.cs
+++ main/Program.cs
@@ -1,71 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO.Pipes;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.InteropServices.Marshalling;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using System.Text.Json;
 using HtmlAgilityPack;
-
-class Product
-{
-    public Product(string name, int price,string type)
-    {
-        Name = name;
-        Price = price;
-        Type = type;
-    }
-    public string Name { get;set; }
-    public int Price { get;set; }
-    public string Type { get; set; }
-}
+using main;
 
 class Program {
-    static readonly string url = "https://telemart.ua/ua/city-1482/ram/";
-
     static async Task Main()
     {
-        List<Product> products = await GetProducts();
+        List<Product> products = await Parser.GetProducts();
         foreach (var product in products)
         {
             Console.WriteLine($"Name: {product.Name}, Price: {product.Price} , Type: {product.Type}");
         }
-    }
-    static async Task<List<Product>> GetProducts()
-    {
-        List<Product> products = new List<Product>();
-        using (var httpClient = new HttpClient())
+        await JSONProduct.Serialize(products);
+        List<Product> newProducts = await JSONProduct.Deserialize();
+        foreach (var product in newProducts)
         {
-            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-            try
-            {
-                var html = await httpClient.GetStringAsync(url);
-                Console.WriteLine("Waiting for response");
-                var htmlDoc = new HtmlDocument();
-                htmlDoc.LoadHtml(html);
-                var productNodes = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'product-item__inner')]");
-
-                if (productNodes != null)
-                {
-                    foreach (var productNode in productNodes)
-                    {
-                        string name = productNode.GetAttributeValue("data-prod-name", "Назву не знайдено");
-                        string price = productNode.GetAttributeValue("data-prod-price", "0");
-                        int type = productNode.GetAttributeValue("data-hd-id_category", 0);
-                        switch (type)
-                        {
-                            case 403: products.Add(new Product(name, int.Parse(price), "Оперативна пам'ять")); break;
-                            default: products.Add(new Product(name, int.Parse(price), "Невідомий продукт")); break;
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return products;
+            Console.WriteLine($"Name: {product.Name}, Price: {product.Price} , Type: {product.Type}");
         }
     }
 }
